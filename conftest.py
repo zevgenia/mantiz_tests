089e3410353e38d58b4d6361f1f0976d05bdbb2f
Evgenia Zatonskaya <zatonskaya@yandex.ru>
2018-09-26 18:06:18 +0300

    Попытка отключить captcha

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ import json
 import pytest
 from fixture.application import Application
 import os.path
+import ftputil
+import ftplib
 
 fixture = None
 target = None
@@ -16,14 +18,46 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config["baseURL"])
+        fixture = Application(browser=browser, base_url=config['web']["baseURL"])
     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    try:
+     remote = ftputil.FTPHost(host, username, password)
+     if remote.path.isfile("config_inc.php.bak"):
+         remote.remove("config_inc.php.bak")
+     if remote.path.isfile("config_inc.php"):
+        remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resourses/config_inc.php"), "config_inc.php")
+    except:
+        return EOFError
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -33,7 +67,6 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
-
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
