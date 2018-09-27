c384d5c606c12c199173eab580fcdec3bce2d825
Evgenia Zatonskaya <zatonskaya@yandex.ru>
2018-09-27 18:01:53 +0300

    Добавлен новый конфиг.файл mantis с переназначенным портом почтового сервера - <smtpserver enabled='true'><port>25</port> в файле файл james-2.3.1\apps\james\SAR-INF\config.xml

---
+++ resourses/config_inc_new.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_use_captcha = 'off';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+$g_smtp_port = 2525;
